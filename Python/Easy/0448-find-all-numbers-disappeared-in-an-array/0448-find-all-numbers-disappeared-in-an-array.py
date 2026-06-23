d9ddbcb93c4d77b2b8a4dd8510c77239cd83791b
Sachin M <68294768+sachin2003226@users.noreply.github.com>
2026-06-23 23:30:08 +0530

    23 ms (98.46%) 26.7 MB (72.75%) 06-23-2026 0448-find-all-numbers-disappeared-in-an-array Easy .py Array

---
+++ Python/Easy/0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        set_nums = set(nums)
+        res = []
+
+        for i in range(1, len(nums) + 1):
+
+            if i not in set_nums:
+                res.append(i)
+
+        return res
